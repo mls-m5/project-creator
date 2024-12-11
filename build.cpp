252bb40dbfb529edc548fdb26c8b90f73dd4298f
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-12-11 22:55:07 +0100

    Add matmake4 file for building

---
+++ build.cpp
@@ -0,0 +1,25 @@
+
+#include "matmake4/matmake.h"
+
+void build() {
+    add_subdirectory("lib");
+
+    executable("create-project") //
+        .src("src/create/*.cpp")
+        .include("include")
+        .copy("README.md");
+
+    executable("build") //
+        .src("src/build/*.cpp")
+        .include("include");
+
+    executable("mfind") //
+        .src("src/find/*.cpp")
+        .flags("-std=c++23")
+        .include("include");
+
+    executable("mopen") //
+        .src("src/open/*.cpp")
+        .flags("-std=c++23")
+        .include("include");
+}
